f2a37966012edf4902c77553709871241e142981
NeatuR <63652413+NeatuR@users.noreply.github.com>
2022-02-15 20:50:40 +0000

    reduce file size by removing white space

--- printLocus.sh
+++ printLocus.sh
@@ -4,17 +4,12 @@
 
 #Aim: given a big VCF file, you can skip to a certain variant and print the following N columns
 
-
-
 #type the positions of the variants between speechmarks (!no space after the position, each one starts with a new line)
 positionList="1602429
 1602430
 1602431"
 
-
-
 while IFS= read -r position; do
-
 
 #if the vcf is compressed (vcf.gz), use "zgrep"; otherwise use "grep";
 #the number at the end of the line denotes the number of columns that will be printed, customise it as desired
